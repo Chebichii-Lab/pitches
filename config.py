1da39ce5830a680cb976470a5b2960e4e9bdda11
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-06-14 11:07:54 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -28,7 +28,8 @@ class ProdConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    pass
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+       SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://")
 
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:chebichii1@localhost/pitches_test'
